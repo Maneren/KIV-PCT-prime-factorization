540710e5ecf07e6c7b69451a2017a4f01af2225d
Maneren <49210777+Maneren@users.noreply.github.com>
2024-04-04 12:33:54 +0200

    add prime sieve

--- main.s
+++ main.s
@@ -19,6 +19,12 @@ rs:
 	.data
 	.align 2
 
+sieve:
+	.space 256
+
+primes:
+	.space 64
+
 	; TODO: revert back to reading the input
 
 input_buffer:
@@ -58,21 +64,32 @@ stack:
 _start:
 	mov.l #stack, ER7
 
+	;     initialize the sieve
+	mov.l #sieve, ER6
+	mov.l #primes, ER5
+	jsr   @generate_prime_sieve
+
+	; loop for the rest of the program
+
+main_loop:
 	;     show prompt
 	mov.w #PUTS, R0
 	mov.l #ptr_prompt, ER1
 	jsr   @syscall
 
-	; read input
-	; mov.w #GETS, R0
-	; mov.l #ptr_input, ER1
-	; jsr   @syscall
+	;     read input
+	mov.w #GETS, R0
+	mov.l #ptr_input, ER1
+	jsr   @syscall
 
 	;     IO is in base 10
 	mov.w #10, R1
 
 	mov.l @ptr_input, ER6
 	jsr   @ascii_decode
+
+	cmp.w #2, R0
+	blt   main_loop
 
 	mov.l @ptr_output, ER6
 	jsr   @ascii_encode
@@ -86,9 +103,133 @@ _start:
 	mov.l #ptr_output, ER1
 	jsr   @syscall
 
+	jmp main_loop
+
 end:
-	;   end with a infinite loop
+	;   end with an infinite loop
 	jmp @end
+
+	; ----------- functions -----------
+
+	; fn generate_prime_sieve
+
+	; generates a prime sieve from 0 to 255
+	; fills the output buffer with found primes, leaving the rest empty
+
+	; <- @ER5 - pointer to output buffer (64B)
+	; <- @ER6 - pointer to working buffer (256B)
+
+generate_prime_sieve:
+	push.l ER0
+	push.l ER1
+	push.l ER2
+
+	;      save the pointer
+	push.l ER6
+
+	;     start iteration at 2
+	mov.w #2, E0
+	inc.l #2, ER6
+
+	;     filler value
+	mov.w #0xFFFF, R0
+
+generate_prime_sieve_fill_loop:
+	; set all bytes in sieve to 1
+
+	;     loop until the counter is 256
+	cmp.w #256, E0
+	bge   generate_prime_sieve_fill_loop_end
+
+	;     store the byte
+	mov.w R0, @ER6
+
+	;     increase the pointer
+	inc.l #2, ER6
+
+	;     increase the counter
+	add.w #2, E0
+
+	jmp generate_prime_sieve_fill_loop
+
+generate_prime_sieve_fill_loop_end:
+
+	;     reset the pointer
+	pop.l ER6
+
+	;     clear iterator and filler
+	xor.l ER0, ER0
+
+	;     start at 2 and mark all multiples as 0
+	mov.b #2, R0L
+	add.l #2, ER6
+
+	;     prepare R1 (copy space)
+	xor.w R1, R1
+
+generate_prime_sieve_loop:
+	;     check if the number is marked as prime
+	mov.b @ER6, R1L
+	cmp.b #0, R1L
+	;     if it isn't, go to the next number
+	beq   generate_prime_sieve_loop_skip
+
+	mov.b R0L, @ER5
+	inc.l #1, ER5
+
+	jsr @generate_prime_sieve_mark_multiples
+
+generate_prime_sieve_loop_skip:
+
+	cmp.w #255, R0
+	bge   generate_prime_sieve_end
+
+	;     increase the number until we reach 256
+	inc.b R0L
+	inc.l #1, ER6
+
+	jmp generate_prime_sieve_loop
+
+generate_prime_sieve_end:
+	pop.l ER2
+	pop.l ER1
+	pop.l ER0
+
+	rts
+
+	; fn generate_prime_sieve_mark_multiples
+
+	; marks all multiples of the number as 0
+
+	; !!! overwrites ER0-2
+
+	; <- R0 - number
+	; <- @ER6 - pointer to the buffer + the number
+
+generate_prime_sieve_mark_multiples:
+	;      store the original pointer
+	push.l ER6
+
+	;     save current number for iteration
+	mov.w R0, E1
+
+generate_prime_sieve_mark_loop:
+	;     step through the sieve in multiples of the current number
+	;     until we reach the end of the sieve (256)
+	add.l ER0, ER6
+	add.w R0, E1
+	cmp.w #256, E1
+	bge   generate_prime_sieve_mark_loop_end
+
+	;     mark them as 0
+	mov.b R1H, @ER6
+
+	jmp generate_prime_sieve_mark_loop
+
+generate_prime_sieve_mark_loop_end:
+	;     restore the original pointer
+	pop.l ER6
+	rts
 
 	; fn ascii_decode
 
